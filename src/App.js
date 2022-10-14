91d7f3e5a1878c60eb98600d8fd6941564dccd8a
AWeidenkopf <oliveira.anacalado@gmail.com>
2022-10-13 20:17:23 -0400

    implement routes and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import About from './Pages/About/About'
+import Contact from './Pages/Contact/Contact'
+import Resume from './Pages/Resume/Resume'
+import Projects from './Pages/Projects/Projects' 
+import NavBar from './Components/NavBar/NavBar'
 
 function App() {
   return (
-    <div className="App">
-      <h1>ABOUT ME</h1>
-    </div>
+    <>
+    <NavBar/>
+    <Routes>
+      <Route 
+      path='/' 
+      element={<Home />} 
+      />
+
+      <Route 
+      path='about' 
+      element={<About />} 
+      />
+
+      <Route 
+      path='contact' 
+      element={<Contact />} 
+      />
+
+      <Route 
+      path='resume' 
+      element={<Resume />} 
+      />
+      <Route 
+      path='projects' 
+      element={<Projects />} 
+      />
+    </Routes>
+    </>
   )
 }
 
